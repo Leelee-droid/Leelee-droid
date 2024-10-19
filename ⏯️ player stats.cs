7ebf52593cc2210111290944768780205202cf29
Leelee-droid <176976371+Leelee-droid@users.noreply.github.com>
2024-10-19 18:02:38 +1000

    Update

---
+++ ⏯️ player stats.cs
@@ -0,0 +1,11 @@
+<!__and__>. I  REMEMBER (!...MEANS NOT EQUAL TO"FALSE",,,...HENCE THE LOOP STOP 🛑 S.
+
+again tell urself parent,<htm1>...</,html>
+                   the child is typed as <head> ...<head>...</head>
+
+<!__<h1> hello world example</h1>__>
+remember Monica save each Ur files as htm or"htm" extension.
+(ui) means an orderered ( or specific) LIST...( EG WHEN U SCROLL DOWN TO SEE TEACHER ❣️ ‼️ MARK'S UBER/ DOOR DASH ETC AD. MATERIAL S( IN CONNECT NOTES).  ...I NEED TO THINK OF THE VARIANLE S LIKE A BANK DEPOSITING AN AMT. TO BE KEPT SAFE( OR JUST ORDERING WHAT I LIKE TO SEE ON THE MENU( IE .AS ME THE CLIENT...AS SAME WITH VIDEO U WATCH ON WORKFLOW API DIAG'& THEN PLACING THEM MY ORDERS ( LISTS ) INTO A TRACE"TABLE"..TO PRESENT 🎁 LAYER TO THE HEAD OF PROJECT DESIGN/ THE DEVELOPER!!!...IN REAL LIFE SCENARIO.
+use- cart.cs 
+try find out next how Ur layout ( image- style) of 
+check out. ..( IE pay here/ 🤪 now button ✅)
